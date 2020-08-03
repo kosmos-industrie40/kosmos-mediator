27fce8074c877e5bf14a85e0e7f6606c8759ada7
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-08-03 16:10:31 +0200

    increase log output

--- models/SensorUpdate.go
+++ models/SensorUpdate.go
@@ -17,10 +17,9 @@ type SensorUpdate struct {
 }
 
 func (s SensorUpdate) Insert(db *sql.DB, machine string, sensor string) error {
-	klog.Infof("identify sensor id")
 	result, err := db.Query("SELECT id FROM machine_sensor JOIN sensor ON sensor.id = machine_sensor.sensor WHERE sensor.transmitted_id = $1 AND machine_sensor.machine = $2", sensor, machine)
 	if err != nil {
-		klog.Errorf("could not query id from machine_sensor; %s\n", err)
+		return err
 	}
 
 	defer func() {
@@ -34,14 +33,13 @@ func (s SensorUpdate) Insert(db *sql.DB, machine string, sensor string) error {
 	// no result will be found
 	if !result.Next() {
 		//TODO error handling; we want to store all sensor data?
-		return fmt.Errorf("could not find machine-sensor combination")
+		return fmt.Errorf("could not find machine-sensor combination; machine id is %s and transmitted sensor id is: %s\n", machine, sensor)
 	}
 
 	if err := result.Scan(&id); err != nil {
 		return fmt.Errorf("could not parse machine-sensor.id to int64: %s\n", err)
 	}
 
-	klog.Infof("insert sensor update data into database")
 	if _, err := db.Exec(fmt.Sprintf("INSERT INTO update_message (sensor_machine, timestamp, meta, column, data, signature) VALUES ($1, $2, $3, $4, $5, $6)"), id, s.Timestamp, s.Meta, s.Columns, s.Data, s.Signature); err != nil {
 		return fmt.Errorf("could not insert update_message data: %s\n", err)
 	}
