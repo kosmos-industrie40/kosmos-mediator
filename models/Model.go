141353d4792b7496884296302dfa78405eecee1a
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-08-13 11:49:41 +0200

    bug fix and increas loggin

--- models/Model.go
+++ models/Model.go
@@ -38,23 +38,37 @@ func (m Model) InitialPipeline(db *sql.DB, machine, sensor, contract string) (Mo
 func (m Model) TestEnd(db *sql.DB, machine, sensor, contract string) (bool, error) {
 	machSens, err := getMachineSensorId(db, machine, sensor)
 	if err != nil {
+		klog.V(2).Infof("error in getMachineSensorId")
 		return false, err
 	}
+	klog.V(2).Infof("machine-sensor id is %d\n", machSens)
 
 	prevModel, err := m.getIdModel(db)
 	if err != nil {
 		return false, err
 	}
-	query, err := db.Query("SELECT EXISTS (SELECT 1 FROM model JOIN next_analyses ON next_analyses.next_model = model.id JOIN pipeline ON pipeline.next_analyses = next_analyses.id WHERE pipeline.contract = $1 AND next_analyses.machine_sensor = $2 AND next_analyses.previous_model = $3", contract, machSens, prevModel)
+
+	klog.V(2).Infof("parameter to use in query are: contract: %s; machine-sensor: %d, prevModel: %d", contract, machSens, prevModel)
+	res, err := db.Query("SELECT EXISTS (SELECT next_model FROM next_analyses JOIN pipeline ON pipeline.analyses = next_analyses.id WHERE pipeline.contract = $1 AND next_analyses.machine_sensor = $2 AND next_analyses.previous_model = $3)", contract, machSens, prevModel)
 	if err != nil {
 		return false, err
 	}
 
-	var exists bool
+	defer func() {
+		if err := res.Close(); err != nil {
+			klog.Errorf("can not close rows: %s\n", err)
+		}
+	}()
 
-	query.Next()
-	err = query.Scan(&exists)
-	return exists, err
+	var ex bool
+
+	for res.Next() {
+		if err := res.Scan(&ex); err != nil {
+			klog.Errorf("cannot scan err is: %s\n", err)
+		}
+
+	}
+	return ex, nil
 }
 
 // Next query the next model based on the current model
@@ -81,24 +95,40 @@ func (m Model) Next(db *sql.DB, machine, sensor, contract string) (Model, error)
 	}()
 
 	var url, tag string
+	klog.Infof("url is %s and tag is %s\n", url, tag)
 
 	query.Next()
 	err = query.Scan(&url, &tag)
+	klog.Infof("url is %s and tag is %s\n", url, tag)
 	return Model{Url: url, Tag: tag}, err
 }
 
 // getIdModel find the id of a model based on the model url and tag
 func (m Model) getIdModel(db *sql.DB) (int64, error) {
+
+	klog.V(2).Infof("Query Parameter in getIdmodel: URL: %s, Tag: %s\n", m.Url, m.Tag)
+
 	query, err := db.Query("SELECT id FROM model WHERE url = $1 AND tag = $2", m.Url, m.Tag)
 	if err != nil {
 		return -1, err
 	}
+	defer func() {
+		if err := query.Close(); err != nil {
+			klog.Errorf("cannot close query object :%s\n", err)
+		}
+	}()
 
 	var id int64
-	query.Next()
-	err = query.Scan(&id)
+	for query.Next() {
+		err = query.Scan(&id)
+		if err != nil {
+			klog.Errorf("cannot scan err: %s\n", err)
+		}
+	}
 
-	return id, err
+	klog.V(2).Infof("id is: %d\n", id)
+
+	return id, nil
 }
 
 // getMachineSensorId find id of the machine_sensor table to a given machine and sensor
